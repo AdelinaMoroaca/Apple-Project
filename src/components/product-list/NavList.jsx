e08a79ec7f345cd023909c4647ca29733df3ccdc
AdelinaMoroaca <moroacaadelina@gmail.com>
2024-08-30 13:36:06 +0300

    Add caroussel for navlist

--- src/components/product-list/NavList.jsx
+++ src/components/product-list/NavList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Col, Row, Container, Spinner, Figure } from 'react-bootstrap';
+import { Container, Spinner, Figure } from 'react-bootstrap';
 import { useFetchData } from '../../utils/hooks/useFetch';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/a11y';
+import { Navigation, Scrollbar, A11y } from 'swiper/modules';
+import styles from './NavList.module.css';
+
 
 function NavList({ categoryProductsId }) {
     const apiUrl = `https://json-server-deployment-5til.onrender.com/nav?categoryProductsId=${categoryProductsId}`;
@@ -18,30 +27,73 @@ function NavList({ categoryProductsId }) {
 
     return (
         <Container>
-            <Row>
-                <Col style={{display: "flex", gap: '20px'}}>
-                    {models && models.length > 0 ? (
-                        models.map(model => (
-                            <Figure 
-                                key={model.id}
-                                style={{cursor: 'pointer'}}
-                                onClick={() => {handleProduct(model)}}
-                            >
-                                <img 
-                                    src={model.image} 
-                                    alt={model.modelName} 
-                                    // width={40} 
-                                    height={50} 
-                                />
-                                <Figure.Caption>{model.modelName}</Figure.Caption>
-                            </Figure>
-                        ))
-                    ) : (
-                        <p>No models found for this category.</p>
-                    )}
-                    
-                </Col>
-            </Row>
+            <Swiper
+            spaceBetween={2}
+            slidesPerView={12}
+            navigation={{
+                  nextEl: `.${styles.swiperButtonNext}`,
+                  prevEl: `.${styles.swiperButtonPrev}`,
+            }}
+            pagination={{ clickable: true }}
+            modules={[Navigation, Scrollbar, A11y]}
+            breakpoints={{
+                  320: {
+                    slidesPerView: 2.6,
+                    spaceBetween: 2
+
+                  },
+                  360: {
+                    slidesPerView: 3.6,
+                    spaceBetween: 2
+                  },
+                  400: {
+                    slidesPerView: 4.2,
+                    spaceBetween: 2
+                  },
+                  480: {
+                    slidesPerView: 5.3,
+                    spaceBetween: 2
+                  },
+                  768: {
+                    slidesPerView: 6.2,
+                    spaceBetween: 2
+                  },
+                  1024: {
+                    slidesPerView: 7,
+                    spaceBetween: 2
+                  },
+                  1440: {
+                    slidesPerView: 9,
+                    spaceBetween: 2
+                  }
+                  ,
+                  1540: {
+                    slidesPerView: 12.2,
+                    spaceBetween: 2
+                  }
+               }}
+             >
+             {models && models.length > 0 ? (
+                models.map(model => (
+                  <SwiperSlide key={model.id}>
+                              <Figure 
+                                  key={model.id}
+                                  style={{cursor: 'pointer'}}
+                                  onClick={() => {handleProduct(model)}}
+                              >
+                                  <img 
+                                      src={model.image} 
+                                      alt={model.modelName} 
+                                      height={50} 
+                                  />
+                                  <Figure.Caption>{model.modelName}</Figure.Caption>
+                              </Figure>
+                  </SwiperSlide>
+                  ))
+                ) : (
+                  <p>No models found for this category.</p>
+                )}
+            </Swiper>   
         </Container>
     );
 }
